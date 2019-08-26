8e1fcc95245589b6c34809c31cca9479843aaba5
Sheraz0456 <48226921+Sheraz0456@users.noreply.github.com>
2019-08-26 21:27:58 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var height = $('#header-nav').height();
+    console.log(height);    
+    $(".header").css("margin-bottom", height);
+    $(window).resize(function(){
+        var height = $('#header-nav').height();
+        console.log(height);    
+        
+        $(".header").css("margin-bottom", height);
+      });
+
+    $("#ham-toggle").click(function(e){
+        $(".toggle-menu").css("margin-left", 0);
+        $("html, body").css("overflow-y", "hidden");
+        e.preventDefault();
+    });
+    
+    $(".exit-ham").click(function(){
+        $(".toggle-menu").css("margin-left", '100%');
+        $("html, body").css("overflow-y", "initial");
+    });
+
+});
